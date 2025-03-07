77388f068729639065fec51f375b0148176dc1c8
JoaquinCar <joaquincarmona758@gmail.com>
2025-03-07 11:36:15 -0600

    DonutTile Finished

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -5,10 +5,10 @@ class DonutTab extends StatelessWidget {
   //lista de donas 
   final List donutsOnSale = [
     //[donutFlavor,donutPrice,donutColor,ImageName]
-    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-    ["Strawberry", "45", Colors.pink, "lib/images/strawberry_donut.png"],
-    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    ["Ice Cream","Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry","Dunkin's","45", Colors.pink, "lib/images/strawberry_donut.png"],
+    ["Grape Ape","Krispy Kreme", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco","Dunkin's", "95", Colors.brown, "lib/images/chocolate_donut.png"],
  
   ];
   DonutTab({super.key});
@@ -29,9 +29,10 @@ class DonutTab extends StatelessWidget {
       itemBuilder: (context, index) {
         return DonutTile(
           donutFlavor: donutsOnSale[index][0],
-          donutPrice: donutsOnSale[index][1],
-          donutColor: donutsOnSale[index][2],
-          donutImage: donutsOnSale[index][3],
+          store: donutsOnSale[index][1],
+          donutPrice: donutsOnSale[index][2],
+          donutColor: donutsOnSale[index][3],
+          donutImage: donutsOnSale[index][4],
         );
         
       },
--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
+  final String store;
   final String donutPrice;
   final dynamic donutColor; //dynamic es un tipo de dato que puede ser cualquier tipo de dato
   final String donutImage;
 
   const DonutTile({super.key, 
     required this.donutFlavor,
+    required this.store,
     required this.donutPrice,
     required this.donutColor,
     required this.donutImage,
@@ -39,16 +41,53 @@ class DonutTile extends StatelessWidget {
              ,),
 
            //DonutImage
-           Padding(padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 24),
+           Padding(padding: const EdgeInsets.symmetric(vertical: 5,horizontal: 25),
            child: Image.asset(donutImage),
-           )
+           ),
 
            //info
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '$donutFlavor',
+                  style:const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 4,)
+              ],
+            ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Text(
+                 '$store',
+                 style:const TextStyle(
+                   fontSize: 16,
+                 ),
+               ),
+               const SizedBox(height: 4,),
+            ]
+       ),
+
+           Padding(
+             padding: const EdgeInsets.all(8.0),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: [
+                 Icon(Icons.favorite_border_outlined,size: 30,),
+                 Icon(Icons.add, color: Colors.grey[800],size: 30,),
+               ],
+             ),
+           )
+        ])
+    ));
+
 
            //fav and add icon
-         ],
-       ),
-      ),
-    );
-  }
+
+
+}
 }
